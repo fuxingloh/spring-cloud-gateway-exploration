daeb5da7c00b6fdf3656116c6328d4f17edf6d50
Fuxing Loh <git@fuxing.dev>
2021-01-06 14:15:45 +0800

    Updated service to be more consistent for testing

--- service/src/main/java/dev/fuxing/spring/RandomController.java
+++ service/src/main/java/dev/fuxing/spring/RandomController.java
@@ -3,20 +3,17 @@ package dev.fuxing.spring;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @RestController
-@RequestMapping("/expensive")
-public class ExpensiveController {
+public class RandomController {
 
-    @GetMapping("/{id}")
-    private Mono<String> getEmployeeById(@PathVariable int id) {
-        return Mono.delay(Duration.ofSeconds(2))
-                .map(aLong -> RandomStringUtils.randomAlphanumeric(id));
+    @GetMapping("/length/{length}/delay/{delay}")
+    private Mono<String> get(@PathVariable int length, @PathVariable int delay) {
+        return Mono.delay(Duration.ofMillis(delay))
+                .map(l -> RandomStringUtils.randomAlphanumeric(length));
     }
-
 }
