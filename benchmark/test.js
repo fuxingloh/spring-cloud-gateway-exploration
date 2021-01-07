b3d8e53b0a957bb9093a8507e9da0d54380923fa
Fuxing Loh <git@fuxing.dev>
2021-01-07 14:40:18 +0800

    1k sustained

--- benchmark/test.js
+++ benchmark/test.js
@@ -40,11 +40,11 @@ export default function () {
 
 export const options = {
   stages: [
-    {duration: '1m', target: 300},
+    {duration: '1m', target: 400},
     {duration: '1m', target: 600},
-    {duration: '1m', target: 900},
-    {duration: '1m', target: 1200},
-    {duration: '1m', target: 1200},
+    {duration: '1m', target: 800},
+    {duration: '1m', target: 1000},
+    {duration: '1m', target: 1000},
     {duration: '3m', target: 0},
   ],
 };
