b28a7c1884b63c7458109a33336b319985847afd
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-01-07 14:40:48 +0800

    Updated test parameters (#7)
    * Updated test parameters
    
    * 1k sustained

--- benchmark/test.js
+++ benchmark/test.js
@@ -1,8 +1,5 @@
 import http from 'k6/http';
 import {check, randomSeed} from "k6";
-import {Rate} from "k6/metrics";
-
-const statusFailure = new Rate("status_failure_rate");
 
 randomSeed(0)
 
@@ -36,20 +33,18 @@ function getUrl(distinct) {
 
 export default function () {
   const response = http.get(getUrl(5000));
-
-  statusFailure.add(!check(response, {
+  check(response, {
     "status is 200": (r) => r.status === 200,
-  }))
+  })
 }
 
 export const options = {
   stages: [
-    {duration: '1m', target: 300},
+    {duration: '1m', target: 400},
     {duration: '1m', target: 600},
-    {duration: '1m', target: 900},
-    {duration: '1m', target: 1200},
-    {duration: '1m', target: 1500},
-    {duration: '1m', target: 1800},
+    {duration: '1m', target: 800},
+    {duration: '1m', target: 1000},
+    {duration: '1m', target: 1000},
     {duration: '3m', target: 0},
   ],
 };
