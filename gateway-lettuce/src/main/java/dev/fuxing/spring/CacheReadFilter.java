0f80dadaff42902296feeea8b2d89e623ffc12f8
Fuxing Loh <git@fuxing.dev>
2021-01-08 18:54:28 +0800

    Reverse the flow of events for lettuce as well

--- gateway-lettuce/src/main/java/dev/fuxing/spring/CacheReadFilter.java
+++ gateway-lettuce/src/main/java/dev/fuxing/spring/CacheReadFilter.java
@@ -4,16 +4,20 @@ import io.lettuce.core.api.reactive.RedisStringReactiveCommands;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.TimeoutException;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.nio.ByteBuffer;
+import java.time.Duration;
 
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.setAlreadyRouted;
 
@@ -36,21 +40,22 @@ public class CacheReadFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String key = exchange.getAttribute(CacheKeyFilter.CACHE_KEY_ATTR);
 
-        return client.get(key)
-                .onErrorResume(throwable -> Mono.empty())
-                .switchIfEmpty(chain.filter(exchange).then(Mono.empty()))
-                .flatMap(buffer -> {
+        Mono<DataBuffer> response = client.get(key)
+                .map(buffer -> exchange.getResponse().bufferFactory().wrap(buffer))
+                .timeout(Duration.ofSeconds(30), Mono.error(new TimeoutException()))
+                .onErrorMap(TimeoutException.class, th -> new ResponseStatusException(HttpStatus.GATEWAY_TIMEOUT, th.getMessage(), th));
+
+        return response
+                .flatMap(dataBuffer -> {
                     setAlreadyRouted(exchange);
                     exchange.getAttributes().put(CacheReadFilter.class.getName(), true);
 
-                    return chain.filter(exchange).then(Mono.defer(() -> {
-                        ServerHttpResponse response = exchange.getResponse();
-                        DataBuffer dataBuffer = response.bufferFactory().wrap(buffer);
+                    HttpHeaders headers = exchange.getResponse().getHeaders();
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                    headers.setContentLength(dataBuffer.readableByteCount());
 
-                        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-                        response.getHeaders().setContentLength(dataBuffer.readableByteCount());
-                        return response.writeWith(Flux.just(dataBuffer));
-                    }));
-                });
+                    return exchange.getResponse().writeWith(Flux.just(dataBuffer));
+                })
+                .then(chain.filter(exchange));
     }
 }
